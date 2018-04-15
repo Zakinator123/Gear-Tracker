cc94c4c05a9896077bc77d6acbd9f9817e6a90ef
Zakey Faieq <zaf2xk@virginia.edu>
2018-04-15 14:52:55 -0400

    Added query by id

--- app/main.py
+++ app/main.py
@@ -7,21 +7,28 @@ import os
 app = Flask(__name__)
 
 @app.route("/")
-def test_database_connections():
+def home():
+    return "Hello World"
+
+
+@app.route("/gear/<number>")
+def get_gear(number):
     db = MySQLdb.connect(os.environ['AWS_DB_HOST'], os.environ['AWS_DB_USER'], os.environ['AWS_DB_PASS'], os.environ['AWS_DB_DATABASE'])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM gear WHERE number=890")
+    cursor.execute("SELECT * FROM gear WHERE number=" + number + ";")
     data1 = cursor.fetchone()
     db.close()
 
+    return jsonify(data1)
 
+@app.route("/user/<id>")
+def get_user(id):
     db = MySQLdb.connect(os.environ['ODC_DB_HOST'], os.environ['ODC_DB_USER'], os.environ['ODC_DB_PASS'], os.environ['ODC_DB_DATABASE'])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM m_member WHERE c_uid=6983")
+    cursor.execute("SELECT * FROM m_member WHERE c_uid=" + id + ";")
     data2=cursor.fetchone()
 
-
-    return jsonify([data1, data2])
+    return jsonify(data2)
 
 if __name__ == "__main__":
     # Only for debugging while developing
