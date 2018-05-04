586971a4c36d1a3d425878bbf6806e4fcad6f3ec
Zakey Faieq <zaf2xk@virginia.edu>
2018-05-03 22:17:26 -0400

    View gear inventory enabled for non-authenticated users

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,54 +16,54 @@ class App extends Component {
         }
     }
 
-    // componentDidMount() {
-    //     fetch('http://192.168.99.100:5000/gear/all')
-    //         .then((response) => {
-    //             return response.json();
-    //         })
-    //         .then((myJson) => {
-    //
-    //             console.log(myJson.length);
-    //
-    //             //Needs to be converted to a map function
-    //             for (let i = 0; i < myJson.length; i++)
-    //             {
-    //                 myJson[i]['id'] = i;
-    //
-    //                 switch(parseInt(myJson[i]['condition_level'])) {
-    //                     case 0:
-    //                         myJson[i]['condition_level'] = "Brand New";
-    //                         break;
-    //                     case 1:
-    //                         myJson[i]['condition_level'] = "Good";
-    //                         break;
-    //                     case 2:
-    //                         myJson[i]['condition_level'] = "Fair";
-    //                         break;
-    //                     case 3:
-    //                         myJson[i]['condition_level'] = "Poor";
-    //                         break;
-    //                 }
-    //
-    //                 switch(myJson[i]['status_level']) {
-    //                     case 0:
-    //                         myJson[i]['status_level'] = "Checked In";
-    //                         break;
-    //                     case 1:
-    //                         myJson[i]['status_level'] = "Checked Out";
-    //                         break;
-    //                     case 2:
-    //                         myJson[i]['status_level'] = "Missing";
-    //                         break;
-    //                     case 3:
-    //                         myJson[i]['status_level'] = "Permanent";
-    //                         break;
-    //                 }
-    //             }
-    //             console.log(myJson);
-    //             this.setState({data: myJson, loading: false});
-    //         });
-    // }
+    componentDidMount() {
+        fetch('http://gear-app.com:5000/gear/all')
+            .then((response) => {
+                return response.json();
+            })
+            .then((myJson) => {
+
+                console.log(myJson.length);
+
+                //Needs to be converted to a map function
+                for (let i = 0; i < myJson.length; i++)
+                {
+                    myJson[i]['id'] = i;
+
+                    switch(parseInt(myJson[i]['condition_level'])) {
+                        case 0:
+                            myJson[i]['condition_level'] = "Brand New";
+                            break;
+                        case 1:
+                            myJson[i]['condition_level'] = "Good";
+                            break;
+                        case 2:
+                            myJson[i]['condition_level'] = "Fair";
+                            break;
+                        case 3:
+                            myJson[i]['condition_level'] = "Poor";
+                            break;
+                    }
+
+                    switch(myJson[i]['status_level']) {
+                        case 0:
+                            myJson[i]['status_level'] = "Checked In";
+                            break;
+                        case 1:
+                            myJson[i]['status_level'] = "Checked Out";
+                            break;
+                        case 2:
+                            myJson[i]['status_level'] = "Missing";
+                            break;
+                        case 3:
+                            myJson[i]['status_level'] = "Permanent";
+                            break;
+                    }
+                }
+                console.log(myJson);
+                this.setState({data: myJson, loading: false});
+            });
+    }
 
     render() {
 
@@ -81,7 +81,7 @@ class App extends Component {
             return (
                 <div className="App-Container">
                     <TopBar />
-                    {/*<EnhancedTable  data={this.state.data}/>*/}
+                    <EnhancedTable  data={this.state.data}/>
                     <BottomBar />
                 </div>
             );
