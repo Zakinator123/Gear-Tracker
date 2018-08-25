523b8ca3570a5211d36a9749171f11e906f477ed
Zakey Faieq <zakeyfaieq@WTA-MBP433.local>
2018-08-24 20:16:48 -0400

    Changed name from gear-app to Gear Tracker throughout code

--- front-end/src/Layouts/BottomBar.js
+++ front-end/src/Layouts/BottomBar.js
@@ -16,8 +16,8 @@ const styles = {
 export const BottomBar = () => {
     return (
             <div className="App-footer">
-                <a href="https://github.com/Zakinator123/Gear-App/" ><div className="Author-text">Gear-App by Zakey Faieq</div></a>
-                <a href="https://github.com/Zakinator123/Gear-App/"> <img className="Github" src={GitHubLogo} alt="Github"/></a>
+                <a href="https://github.com/Zakinator123/Gear-Tracker/" ><div className="Author-text">Gear Tracker by Zakey Faieq</div></a>
+                <a href="https://github.com/Zakinator123/Gear-Tracker/"> <img className="Github" src={GitHubLogo} alt="Github"/></a>
             </div>
     )
 };
